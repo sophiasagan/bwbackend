1076e5bcc0b4dc71f42478362108c69c723c4cec
sophiasagan <sophiabraddock82@gmail.com>
2020-05-20 22:57:11 -0400

    commit for 5-20

--- sleep/sleep-model.js
+++ sleep/sleep-model.js
@@ -5,9 +5,9 @@ module.exports = {
     getSleepDataByUser,
     updateSleepData,
     removeSleepData,
-    getDuration,
-    getAvgSleepData,
-    getSleepScore,
+    // getDuration,
+    // getAvgSleepData,
+    // getSleepScore,
 };
 
 
@@ -33,34 +33,34 @@ function removeSleepData(id) {
         .delete();
 }
 
-function getDuration(sleep) {
-    const { sleep_start, sleep_end } = sleep
-    return (new Date(sleep_start) - new Date(sleep_end)) / 60 / 60 / 1000
-}
+// function getDuration(sleep) {
+//     const { sleep_start, sleep_end } = sleep
+//     return (new Date(sleep_start) - new Date(sleep_end)) / 60 / 60 / 1000
+// }
 
-function getSleepScore(sleep) {
-    console.log(sleep)
+// function getSleepScore(sleep) {
+//     console.log(sleep)
 
-    const { moodBeforeSleep, moodAfterSleep } = sleep
-    return (moodAfterSleep + moodBeforeSleep) / 2
-}
+//     const { moodBeforeSleep, moodAfterSleep } = sleep
+//     return (moodAfterSleep + moodBeforeSleep) / 2
+// }
 
-async function getAvgSleepData(id) {
-    let today = new Date();
-    let sevenDays = new Date();
-    sevenDays.setDate(sevenDays.getDate() - 6);
-    today = dateFormat(today, 'yyyy-mm-dd');
-    sevenDays = dateFormat(sevenDays, 'yyyy-mm-dd');
-    const average = await db('sleep')
-        .avg('timeSlept as avgTimeSlept')
-        .avg('moodAfterSleep as postMood')
-        .avg('sleepMood as avgSleepMood')
-        .whereBetween('date', [sevenDays, today])
-        .andWhere('user_id', id)
-        .first();
-    return {
-        avgTimeSlept: Math.round(average.avgTimeSlept),
-        postMood: Math.round(average.postMood),
-        avgSleepMood: Math.round(average.avgSleepMood),
-    };
-}
+// async function getAvgSleepData(id) {
+//     let today = new Date();
+//     let sevenDays = new Date();
+//     sevenDays.setDate(sevenDays.getDate() - 6);
+//     today = dateFormat(today, 'yyyy-mm-dd');
+//     sevenDays = dateFormat(sevenDays, 'yyyy-mm-dd');
+//     const average = await db('sleep')
+//         .avg('timeSlept as avgTimeSlept')
+//         .avg('moodAfterSleep as postMood')
+//         .avg('sleepMood as avgSleepMood')
+//         .whereBetween('date', [sevenDays, today])
+//         .andWhere('user_id', id)
+//         .first();
+//     return {
+//         avgTimeSlept: Math.round(average.avgTimeSlept),
+//         postMood: Math.round(average.postMood),
+//         avgSleepMood: Math.round(average.avgSleepMood),
+//     };
+// }
